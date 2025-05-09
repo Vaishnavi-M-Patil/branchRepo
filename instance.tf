0009402af742fd9ce24a36ebd99ef37a45aa8fbd
Vaishnavi <vaishnavipatil6002@gmail.com>
2025-05-09 22:05:29 +0530

    All vpc and subnet configuration is working properly

--- instance.tf
+++ instance.tf
@@ -1,21 +1,21 @@
-resource "aws_instance" "ec2inst" {
-    ami = "ami-084568db4383264d4"
-    key_name = "vir-key"
-    instance_type = "t2.micro"
-    subnet_id = aws_subnet.pub_subnet.id
-    security_groups = ["aws_security_group.newSecuritygrp.id"] 
-    tags = {
-      Name = "First-instance"
-    }
-}
-
-resource "aws_instance" "ec2instance" {
+resource "aws_instance" "pvtInst" {
     ami = "ami-084568db4383264d4"
     key_name = "vir-key"
     instance_type = "t2.micro"
     subnet_id = aws_subnet.pvt_subnet.id
-    vpc_security_group_ids = ["aws_security_group.newSecuritygrp.id"]
+    vpc_security_group_ids = [ aws_security_group.newSecuritygrp.id ]
     tags = {
-      Name = "First-instance"
+      Name = "private-instance"
+    }
+}
+
+resource "aws_instance" "pubInst" {
+    ami = "ami-084568db4383264d4"
+    key_name = "vir-key"
+    instance_type = "t2.micro"
+    subnet_id = aws_subnet.pub_subnet.id
+    vpc_security_group_ids = [ aws_security_group.newSecuritygrp.id ]
+    tags = {
+      Name = "public-instance"
     }
 }
